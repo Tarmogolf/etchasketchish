8e01a1889069e73a76b7ed1fa15113af4ee500cb
Stan <stan.bessey@gmail.com>
2015-05-30 20:54:33 -0600

    finished product

--- etch.js
+++ etch.js
@@ -1,14 +1,53 @@
+var red = 0;
+var rDelta = 9;
+var blue = 0;
+var bDelta = 5;
+var green = 0;
+var gDelta = 3;
+
 $(document).ready(function(){
 
-	createTable(60);
+	createTable(16);
+
+	hova();
+
+	$("#reset").click(function(){
+		$("#wrapper").empty();
+		var gridSize = prompt("Enter a grid size (X by X)")
+		createTable(gridSize);
+		hova();
+	});
+
 });
+
+function hova(){
+	$(".box").hover(function(){
+		if(red + rDelta > 255){
+			red = 0;
+		}else{
+			red+=rDelta;
+		}
+		if(blue + bDelta > 255){
+			blue = 0;
+		}else{
+			blue+=bDelta;
+		}
+		if(green + gDelta > 255){
+			green = 0;
+		}else{
+			green+=gDelta;
+		}
+		$(this).css("background-color","rgb("+red+","+green+","+blue+")");
+	});
+};
+
 
 function createTable(numRows){
 
 
 	for(var i = 0; i<numRows; i++){
 		for(var j = 0; j<numRows; j++){
-			var $newDiv = $("<div>" + i+","+j+"</div>");
+			var $newDiv = $("<div></div>");
 			$($newDiv).addClass("box");
 			$("#wrapper").append($newDiv);
 		}
